71923df6af32445a6e48e055ac758e2e5844f75e
Thomas Lohse <thomas.k.lohse@gmail.com>
2022-10-16 18:31:07 +0200

    upd

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -2,27 +2,29 @@ local Remap = require("tlohse.keymap")
 local nnoremap = Remap.nnoremap
 local inoremap = Remap.inoremap
 local nvim_lsp = require('lspconfig')
+local rt = require("rust-tools")
 
 local opts = {
         tools = { -- rust-tools options
         autoSetHints = true,
+        --hover_with_actions = true,
     },
 
     -- all the opts to send to nvim-lspconfig
     -- these override the defaults set by rust-tools.nvim
     -- see https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md#rust_analyzer
-    server = {
-        -- on_attach is a callback called when the language server attachs to the buffer
-        -- on_attach = on_attach,
-        standalone = false,
-        on_attach = function(_, bufnr)
-            -- Hover actions
-            Remap.keymap.set("n", "<C-space>", rt.hover_actions.hover_actions, { buffer = bufnr })
-            -- Code action groups
-            Remap.keymap.set("n", "<Leader>a", rt.code_action_group.code_action_group, { buffer = bufnr })
-        end,
-    },
     settings = {
+        server = {
+            -- on_attach is a callback called when the language server attachs to the buffer
+            -- on_attach = on_attach,
+            standalone = false,
+            on_attach = function(_, bufnr)
+                -- Hover actions
+                vim.keymap.set("n", "<C-space>", rt.hover_actions.hover_actions, { buffer = bufnr })
+                -- Code action groups
+                vim.keymap.set("n", "<Leader>a", rt.code_action_group.code_action_group, { buffer = bufnr })
+            end,
+        },
         -- to enable rust-analyzer settings visit:
         -- https://github.com/rust-analyzer/rust-analyzer/blob/master/docs/user/generated_config.adoc
         ["rust-analyzer"] = {
@@ -34,8 +36,11 @@ local opts = {
     }
 },
 
+Remap.nnoremap("K", ":RustHoverActions<CR>", { silent = true })
+
 require('rust-tools').setup(opts)
 require('rust-tools').inlay_hints.enable()
+require'rust-tools'.hover_range.hover_range()
 
 local cmp = require'cmp'
 cmp.setup({
@@ -58,7 +63,7 @@ cmp.setup({
     ['<CR>'] = cmp.mapping.confirm({
       behavior = cmp.ConfirmBehavior.Insert,
       select = false,
-    })
+    }),
   },
 
   -- Installed sources
