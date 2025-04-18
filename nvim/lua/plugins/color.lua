06c771414d171e45b4498cc475b509aebf71b826
Thomas Lohse <49527735+t-lohse@users.noreply.github.com>
2025-04-18 18:04:03 +0200

    minor changes

--- nvim/lua/plugins/color.lua
+++ nvim/lua/plugins/color.lua
@@ -15,13 +15,38 @@ return {
             require("lavish").apply()
         end,
     },
-    ]] --
     { "lancewilhelm/horizon-extended.nvim", priority = 1000 },
+    --{ "akinsho/horizon.nvim",               version = "*" },
     -- Configure LazyVim to load gruvbox
     {
         "LazyVim/LazyVim",
         opts = {
             colorscheme = "horizon-extended-neo",
+        },
+    },
+    ]] --
+    {
+        "HoNamDuong/hybrid.nvim",
+        lazy = false,
+        priority = 1000,
+        opts = {},
+    },
+    --[[
+    {
+        "rockyzhang24/arctic.nvim",
+        dependencies = { "rktjmp/lush.nvim" },
+        name = "arctic",
+        branch = "main",
+        priority = 1000,
+        config = function()
+            vim.cmd("colorscheme arctic")
+        end
+    },]] --
+    {
+        "LazyVim/LazyVim",
+        opts = {
+            --colorscheme = "arctic",
+            colorscheme = "hybrid",
         },
     },
 }
