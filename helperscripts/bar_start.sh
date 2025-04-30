9fac2f42c0aa927d12a6b8e6f1b3712f3c3581ad
Thomas Lohse <49527735+t-lohse@users.noreply.github.com>
2025-04-30 19:41:19 +0200

    waybar style

--- helperscripts/bar_start.sh
+++ helperscripts/bar_start.sh
@@ -1,4 +1,5 @@
 #! /bin/sh
 #waybar
 #notify-send "$HOME/.config/waybar/$(more /etc/hostname).conf"
-waybar -c "$HOME/.config/waybar/$(more /etc/hostname).conf"
+HOSTNAME=$(more /etc/hostname)
+waybar -c "$HOME/.config/waybar/$HOSTNAME.conf" -s "$HOME/.config/waybar/$HOSTNAME.css"
