66a98e7cf693a75c9321d49b3d167bad57253b7b
Thomas Lohse <49527735+t-lohse@users.noreply.github.com>
2025-05-06 17:34:55 +0200

    better default workspace

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -2,11 +2,12 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 local obj = vim.o
-obj.tabstop = 4      -- A TAB character looks like 4 spaces
+obj.tabstop = 4 -- A TAB character looks like 4 spaces
 obj.expandtab = true -- Pressing the TAB key will insert spaces instead of a TAB character
-obj.softtabstop = 4  -- Number of spaces inserted instead of a TAB character
-obj.shiftwidth = 4   -- Number of spaces inserted when indenting
-vim.opt.clipboard = 'unnamedplus'
+obj.softtabstop = 4 -- Number of spaces inserted instead of a TAB character
+obj.shiftwidth = 4 -- Number of spaces inserted when indenting
+vim.opt.clipboard = "unnamedplus"
 --local opt = vim.opt
 --opt.foldmethod = "expr"
 --opt.foldexpr = "nvim_treesitter#foldexpr()"
+vim.g.riscv_asm_isa = "riscv"
